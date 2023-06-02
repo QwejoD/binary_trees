e3b6ae85fbb3b8b0d818498087e5c1885e964925
QwejoD <desmondqwejo@gmail.com>
2023-06-02 08:37:33 -0700

    task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_is_leaf - a function that checks if a node is a leaf
- * 
+ * no child  
  * @node: node to study
  * Return: return 1 if node is a leaf, otherwise 0
  */
